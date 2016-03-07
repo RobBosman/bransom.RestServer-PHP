34962c0a3721d5ef56c056751d9a0f1086a8b9f6
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-03-07 22:03:34 +0100

    zucht

--- php/nl/bransom/auth/OpenIDConnect/OpenIDConnect.class.php
+++ php/nl/bransom/auth/OpenIDConnect/OpenIDConnect.class.php
@@ -56,9 +56,9 @@ class OpenIDConnect {
             if (!isset($requestState)) {
                 $this->requestAuthCode($redirectUrl, $hostedDomain, $legacyRealm);
             } else if ($requestState != $this->getAntiForgeryStateToken(FALSE)) {
-                self::logErrorAndClearCache("Invalid state parameter: expected\n\t"
+                self::logErrorAndClearCache("Invalid state parameter: expected "
                         . $this->getAntiForgeryStateToken(FALSE)
-                        . " but got\n\t$requestState.\n$_SERVER[REQUEST_URI]");
+                        . " but got $requestState.\n$_SERVER[REQUEST_URI]\n" . print_r($_SESSION));
                 HttpUtil::replyError(401, 'Invalid state parameter');
             } else if (isset($requestCode)) {
                 $jwt = $this->exchangeCodeForJWT($requestCode, $redirectUrl);
