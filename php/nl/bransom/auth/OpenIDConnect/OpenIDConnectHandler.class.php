1cbcaf57627df466a1db2269b1d518da6e5d9eab
Rob Bosman <rob.bosman.nl+github@gmail.com>
2016-03-07 20:34:58 +0100

    minor bugfix

--- php/nl/bransom/auth/OpenIDConnect/OpenIDConnectHandler.class.php
+++ php/nl/bransom/auth/OpenIDConnect/OpenIDConnectHandler.class.php
@@ -13,55 +13,58 @@ Bootstrap::import('nl.bransom.persistency.meta.Schema');
  */
 class OpenIDConnectHandler {
 
-    private $appName;
+  private $appName;
 
-    public function __construct($appName) {
-        $this->appName = $appName;
-    }
+  public function __construct($appName) {
+    $this->appName = $appName;
+  }
 
-    public function getSignedInAccountId(&$jwt) {
-        if ($jwt == NULL) {
-            $jwt = HttpUtil::getJWTFromHeader();
-        }
-        $jwtPayload = OpenIDConnect::getValidatedJWTPayload($jwt);
-        if (isset($jwtPayload->email) && $jwtPayload->email != NULL
-                && isset($jwtPayload->email_verified) && $jwtPayload->email_verified === TRUE) {
-            $emailParts = explode('@', $jwtPayload->email);
-            return $this->getAccountIdByName($emailParts[0]);
-        }
-        return NULL;
+  public function getSignedInAccountId(&$jwt) {
+    if ($jwt == NULL) {
+      $jwt = HttpUtil::getJWTFromHeader();
     }
+    $jwtPayload = OpenIDConnect::getValidatedJWTPayload($jwt);
+    if (isset($jwtPayload->email)
+            && $jwtPayload->email != NULL
+            && isset($jwtPayload->email_verified)
+            && $jwtPayload->email_verified === TRUE) {
+      $emailParts = explode('@', $jwtPayload->email);
+      return $this->getAccountIdByName($emailParts[0]);
+    }
+    return NULL;
+  }
 
-    private function getAccountIdByName($accountName) {
-        $accountIdCacheKey = array(
-            'id' => 'ACCOUND_ID_FOR_' . strtolower($accountName),
-            'exp' => 3600); // 1 hour
-        $accountId = SessionCache::get($accountIdCacheKey);
-        if ($accountId == NULL) {
-            $accountId = $this->getAccountIdByNameFromDB($accountName);
-            SessionCache::set($accountIdCacheKey, $accountId);
-        }
-        return $accountId;
+  private function getAccountIdByName($accountName) {
+    $accountIdCacheKey = array(
+        'id' => 'ACCOUND_ID_FOR_' . strtolower($accountName),
+        'exp' => 3600); // 1 hour
+    $accountId = SessionCache::get($accountIdCacheKey);
+    if ($accountId == NULL) {
+      $accountId = $this->getAccountIdByNameFromDB($accountName);
+      SessionCache::set($accountIdCacheKey, $accountId);
     }
+    return $accountId;
+  }
 
-    private function getAccountIdByNameFromDB($accountName) {
-        try {
-            $schema = MetaData::getInstance()->getSchema($this->appName);
-            $mySQLi = $schema->getMySQLi();
-            $queryString = "SELECT id FROM " . DbConstants::TABLE_ACCOUNT . " WHERE name LIKE '" . $accountName . "'";
-            $queryResult = $mySQLi->query($queryString);
-            if (!$queryResult) {
-                throw new Exception("Error fetching account ID for '$accountName' - $mySQLi->error\n<!--\n$queryString\n-->");
-            }
-            $queryData = $queryResult->fetch_assoc();
-            if (isset($queryData['id'])) {
-                $id = $queryData['id'];
-            }
-            $queryResult->close();
-            return $id;
-        } catch (Exception $e) {
-            Bootstrap::logException($e);
-            return NULL;
-        }
+  private function getAccountIdByNameFromDB($accountName) {
+    $id = NULL;
+    try {
+      $schema = MetaData::getInstance()->getSchema($this->appName);
+      $mySQLi = $schema->getMySQLi();
+      $queryString = "SELECT id FROM " . DbConstants::TABLE_ACCOUNT . " WHERE name LIKE '" . $accountName . "'";
+      $queryResult = $mySQLi->query($queryString);
+      if (!$queryResult) {
+        throw new Exception("Error fetching account ID for '$accountName' - $mySQLi->error\n<!--\n$queryString\n-->");
+      }
+      $queryData = $queryResult->fetch_assoc();
+      if (isset($queryData['id'])) {
+        $id = $queryData['id'];
+      }
+      $queryResult->close();
+    } catch (Exception $e) {
+      Bootstrap::logException($e);
     }
+    return $id;
+  }
+
 }
