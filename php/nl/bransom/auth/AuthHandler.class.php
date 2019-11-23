c8c65b048c9c10aa0e09f6a5d0bd5331db4da80c
RobBosman <rob.bosman.nl+github@gmail.com>
2019-11-23 12:26:35 +0100

    cosmetic

--- php/nl/bransom/auth/AuthHandler.class.php
+++ php/nl/bransom/auth/AuthHandler.class.php
@@ -13,31 +13,31 @@ Bootstrap::startSession();
  */
 class AuthHandler {
 
-    public static function getSignedInAccountId($appName, &$jwt = NULL) {
-        $authModelPropertyName = 'authentication-model-for-app.' . $appName;
-        $authModel = self::getAuthModel($authModelPropertyName);
-        if ($authModel === NULL) {
-            throw new Exception("Config error: please specify property '$authModelPropertyName' in section [auth].");
-        }
-        if (strcasecmp($authModel, 'OpenIDConnect') == 0) {
-            $handler = new OpenIDConnectHandler($appName);
-            return $handler->getSignedInAccountId($jwt);
-        } else if (strcasecmp($authModel, 'HTPassword') == 0) {
-            return HTPasswordHandler::getSignedInAccountId();
-        } else if (strcasecmp($authModel, 'NONE') == 0 || $authModel == '') {
-            return 1;
-        } else {
-            throw new Exception("Config error: unexpected value for property $authModelPropertyName: '$authModel'.");
-        }
+  public static function getSignedInAccountId($appName, &$jwt = NULL) {
+    $authModelPropertyName = 'authentication-model-for-app.' . $appName;
+    $authModel = self::getAuthModel($authModelPropertyName);
+    if ($authModel === NULL) {
+      throw new Exception("Config error: please specify property '$authModelPropertyName' in section [auth].");
     }
+    if (strcasecmp($authModel, 'OpenIDConnect') === 0) {
+      $handler = new OpenIDConnectHandler($appName);
+      return $handler->getSignedInAccountId($jwt);
+    } else if (strcasecmp($authModel, 'HTPassword') === 0) {
+      return HTPasswordHandler::getSignedInAccountId();
+    } else if (strcasecmp($authModel, 'NONE') == 0 || $authModel == '') {
+      return 1;
+    } else {
+      throw new Exception("Config error: unexpected value for property $authModelPropertyName: '$authModel'.");
+    }
+  }
 
-    private static function getAuthModel($authModelPropertyName) {
-        $config = Config::getInstance();
-        foreach ($config->getSection('auth') as $propertyName => $value) {
-            if (strcasecmp($propertyName, $authModelPropertyName) === 0) {
-                return ($value !== NULL ? $value : '');
-            }
-        }
-        return NULL;
+  private static function getAuthModel($authModelPropertyName) {
+    $config = Config::getInstance();
+    foreach ($config->getSection('auth') as $propertyName => $value) {
+      if (strcasecmp($propertyName, $authModelPropertyName) === 0) {
+        return ($value !== NULL ? $value : '');
+      }
     }
+    return NULL;
+  }
 }
